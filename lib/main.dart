c1ccfdbe4d448c37feb5985949609452e8aea1d4
raunak1497 <raunak1497@gmail.com>
2021-04-13 16:01:55 +0530

    Scanner page in progress

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,21 @@
+import 'package:authetify/pages/scanner.dart';
 import 'package:flutter/material.dart';
+import 'package:barcode_scanner/';
 
 void main() {
   //widgetApp //MaterialApp //CupertinoApp
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      home: ScannerPage(),
+      routes: {
+        ScannerPage.routeName: (context) => ScannerPage(),
+      },
+    );
   }
 }
